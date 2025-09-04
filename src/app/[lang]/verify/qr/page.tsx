5c3f3ca36780d82158e2308b564b6fc98addb5e5
[dyad] <samyunwanarmenia@gmail.com>
2025-09-04 07:25:48 +0400

    [dyad] Отключено правило ESLint для самозакрывающихся тегов в файле QR-верификации. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/app/[lang]/verify/qr/page.tsx
+++ src/app/[lang]/verify/qr/page.tsx
@@ -257,7 +257,9 @@ const QrVerifyPage = ({ params: _params }: QrVerifyPageProps) => { // Renamed pa
         )}
         {isRecording && (
           <div className="absolute top-2 left-2 bg-red-600 text-white text-xs px-2 py-1 rounded-full flex items-center">
+            {/* eslint-disable-next-line react/self-closing-comp */}
             <span className="relative flex h-2 w-2 mr-1" />
+            {/* eslint-disable-next-line react/self-closing-comp */}
             <span className="relative inline-flex rounded-full h-2 w-2 bg-white" />
             REC
           </div>
