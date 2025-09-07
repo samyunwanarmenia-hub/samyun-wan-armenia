a5db03490a3f644350b31b0316aa14d35518e214
[dyad] <samyunwanarmenia@gmail.com>
2025-09-07 08:24:43 +0400

    [dyad] Cleaned up unused intro animation JSX and CSS files - wrote 1 file(s), deleted 3 file(s)

--- src/components/IntroAnimation.tsx
+++ src/components/IntroAnimation.tsx
@@ -94,55 +94,6 @@ const IntroAnimation: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="animation06">
-        <div className="rhombus05">
-          <div className="rhombus04">
-            <div className="rhombus03">
-              <div className="rhombus02">
-                <div className="rhombus01"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="animation07">
-        <div className="circle">
-          <div className="circle_element01"></div>
-        </div>
-        <div className="line_wrapper line_wrapper01">
-          <span className="line line01"></span>
-        </div>
-        <div className="rotate60">
-          <div className="line_wrapper line_wrapper02">
-            <span className="line line02"></span>
-          </div>
-        </div>
-        <div className="rotate120">
-          <div className="line_wrapper line_wrapper03">
-            <span className="line line03"></span>
-          </div>
-        </div>
-        <div className="line_wrapper line_wrapper04">
-          <span className="line line04"></span>
-        </div>
-        <div className="rotate-120">
-          <div className="line_wrapper line_wrapper05">
-            <span className="line line05"></span>
-          </div>
-        </div>
-        <div className="rotate-60">
-          <div className="line_wrapper line_wrapper06">
-            <span className="line line06"></span>
-          </div>
-        </div>
-      </div>
-      <div className="animation08">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
     </motion.div>
   );
 };
